234d0aaa24418c651e01c4a87f6d497d4390cb5d
mytabworks <mytabworks@gmail.com>
2020-02-18 21:14:24 +0800

    add types in v 0.1.9

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,37 @@
-declare module 'mytabworks-react-select';
+import React from 'react'
+
+declare module 'mytabworks-react-select'
+
+interface OptValueProps {
+    label: string;
+    value: string;
+}
+
+export interface OptionProps {
+    label: string;
+    value: string | OptValueProps[]
+}
+
+export interface SelectProps { 
+        id:             string;
+        name:           string;
+        placeholder?:   string;
+        className?:     string;
+        onChange?:      (event: any) => void;
+        multiple?:      boolean;
+        disabled?:      boolean;
+        isAutoClear?:   boolean;
+        noDisplayText?: string;
+        isSearch?:      boolean;
+        searchSpeed?:   number;
+        value?:         string | string[];
+        defaultValue?:  string | string[];
+        children:       OptionProps[];
+        isAutoNavigate?: boolean;
+        isClearOptions?: boolean;
+        isShowNoDisplay?:boolean;
+}
+
+declare const Select: React.FunctionComponent<SelectProps> 
+
+export default Select
